27175b6656674e4490e1b63cf15352463145ef68
NonCrede <nikitaomsk03@gmail.com>
2021-08-31 00:29:47 +0600

    Added registration

--- accounts/views.py
+++ accounts/views.py
@@ -21,13 +21,8 @@ class LoginUserView(LoginView):
     form_class = AuthenticationForm
     template_name = 'registration/login.html'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Авторизация")
-        return dict(list(context.items()) + list(c_def.items()))
-
     def get_success_url(self):
-        return reverse_lazy('home')
+        return reverse_lazy('polls:roulette')
 
 
 def logout_user(request):
