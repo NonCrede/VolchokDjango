ea2b5cf4be16a4eb08eea1155be99827d0fc318f
NonCrede <nikitaomsk03@gmail.com>
2021-09-08 23:53:18 +0600

    manytomany

--- packages/models.py
+++ packages/models.py
@@ -8,17 +8,24 @@ from django.views.generic import TemplateView
 
 class Ques(models.Model):
     question_text = models.CharField("Вопрос:", max_length=200, null=True)
-    Ques_id = models.AutoField(auto_created=True, primary_key=True, verbose_name='id')
+    ques_id = models.AutoField(auto_created=True, primary_key=True, verbose_name='id')
+
+    def __str__(self):
+        return self.question_text
 
 
 class Package(models.Model):
     name = models.CharField("Название пака:", max_length=200, null=True)
-    Pack_id = models.AutoField(auto_created=True, primary_key=True, verbose_name='id')
+    Quest = models.ManyToManyField(Ques, null=True)
+    pack_id = models.AutoField(auto_created=True, primary_key=True, verbose_name='id')
+
+    def __str__(self):
+        return self.name
 
 
 class PaQues(models.Model):
-    Pack = models.ManyToManyField(Package)
-    Quest = models.ManyToManyField(Ques)
+    Pack = models.ManyToManyField(Package, null=True)
+    Quest = models.ManyToManyField(Ques, null=True)
 
 
 
